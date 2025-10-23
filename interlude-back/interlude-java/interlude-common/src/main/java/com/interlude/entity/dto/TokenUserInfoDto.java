51265b0f1dd93ea8bb3389e8d347b4eedf348a77
夏代天夜白 <x3319350388@163.com>
2025-10-23 22:46:01 +0800

    当前用户的信息Dto

---
+++ interlude-back/interlude-java/interlude-common/src/main/java/com/interlude/entity/dto/TokenUserInfoDto.java
@@ -0,0 +1,92 @@
+package com.interlude.entity.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TokenUserInfoDto implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String token;
+    private String userId;
+    private String nickName;
+    private String avatar;
+    private Long expireAt;
+
+    private Integer fansCount;
+    private Integer focusCount;
+    private Integer likeCount;
+    private Integer mutualCount;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Long getExpireAt() {
+        return expireAt;
+    }
+
+    public void setExpireAt(Long expireAt) {
+        this.expireAt = expireAt;
+    }
+
+    public Integer getFansCount() {
+        return fansCount;
+    }
+
+    public void setFansCount(Integer fansCount) {
+        this.fansCount = fansCount;
+    }
+
+    public Integer getFocusCount() {
+        return focusCount;
+    }
+
+    public void setFocusCount(Integer focusCount) {
+        this.focusCount = focusCount;
+    }
+
+    public Integer getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(Integer likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public Integer getMutualCount() {
+        return mutualCount;
+    }
+
+    public void setMutualCount(Integer mutualCount) {
+        this.mutualCount = mutualCount;
+    }
+}
