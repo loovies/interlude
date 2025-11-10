570e369a85d1f21ef5d4c94ff4333364d14b5828
夏代天夜白 <x3319350388@163.com>
2025-11-10 22:17:34 +0800

    .

---
+++ interlude-back/interlude-java/interlude-common/src/main/java/com/interlude/utils/JsonUtils.java
@@ -0,0 +1,24 @@
+package com.easylive.utils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
+import java.util.List;
+
+/**
+ * JSON工具类
+ */
+public class JsonUtils {
+    public static String convertObj2Json(Object obj){
+        if(null == obj){
+            return null;
+        }
+
+        return JSON.toJSONString(obj, SerializerFeature.DisableCircularReferenceDetect);
+    }
+
+    public static <T> List<T> convertJsonArray2List(String json, Class<T> clazz){
+        return JSONArray.parseArray(json, clazz);
+    }
+}
